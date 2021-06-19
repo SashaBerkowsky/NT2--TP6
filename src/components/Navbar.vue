2decd8f70bcdfe6d883cdfd074ce9f9879774a5e
Sasha Berkowsky <s.nberkowsky@gmail.com>
2021-06-18 23:31:55 -0300

    .

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -24,17 +24,6 @@
               <a class="nav-link" href="/crearAlumno">Crear alumno</a>
             </li>
           </ul>
-          <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
